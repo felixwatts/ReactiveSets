7fc499d0c9746f9d8fc630ddb2b1c5a8b8ec4073
felixwatts <felixwatts@gmail.com>
2018-10-23 10:31:02 +0200

    added tests for UnionOfSets

--- ReactiveSets.Test/UnionOfSets.cs
+++ ReactiveSets.Test/UnionOfSets.cs
@@ -5,6 +5,7 @@ using Moq;
 using ReactiveSets;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ReactiveSets.Test
 {
@@ -12,7 +13,7 @@ namespace ReactiveSets.Test
     public class UnionOfSets
     {
         private Set<string, Set<string, string>> _input;
-        private ReactiveSets.ISet<string, string> _subject;
+        private ISet<string, string> _subject;
 
 
         [SetUp]
@@ -20,6 +21,196 @@ namespace ReactiveSets.Test
         {
             _input = new Set<string, Set<string, string>>();
             _subject = _input.Union();
+        }
+
+        [Test]
+        public void WhenInputIsEmptyOutputIsEmpty()
+        {
+            _subject.Snapshot(content => Assert.IsEmpty(content));
+        }
+
+        [Test]
+        public async Task SingleInputSet()
+        {
+            var set = new Set<string, string>();
+            set.SetItem("A");
+            set.SetItem("B");
+
+            _input.SetItem("S1", set);
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+        }
+
+        [Test]
+        public async Task MultipleInputSets()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+            set2.SetItem("C");
+            set2.SetItem("D");
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(4, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+            Assert.AreEqual("C", output["C"]);
+            Assert.AreEqual("D", output["D"]);
+        }
+
+        [Test]
+        public async Task RemoveAnInputSet()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+            set2.SetItem("C");
+            set2.SetItem("D");
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            _input.DeleteItem("S1");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("C", output["C"]);
+            Assert.AreEqual("D", output["D"]);
+        }
+
+        [Test]
+        public async Task AddAnItemToAnInputSet()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+
+            _input.SetItem("S1", set1);
+
+            set1.SetItem("B");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+        }
+
+        [Test]
+        public async Task RemoveAnItemFromAnInputSet()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            _input.SetItem("S1", set1);
+
+            set1.DeleteItem("B");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(1, output.Count);
+            Assert.AreEqual("A", output["A"]);
+        }
+
+        [Test]
+        public async Task AddADuplicateItem()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            set2.SetItem("A");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+        }
+
+        [Test]
+        public async Task RemoveADuplicateItem()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+            set2.SetItem("A");
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            set2.DeleteItem("A");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+        }
+
+        [Test]
+        public async Task AddASetWithADuplicateItem()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+            set2.SetItem("B");
+            set2.SetItem("C");
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(3, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
+            Assert.AreEqual("C", output["C"]);
+        }
+
+        [Test]
+        public async Task RemoveASetWithADuplicateItem()
+        {
+            var set1 = new Set<string, string>();
+            set1.SetItem("A");
+            set1.SetItem("B");
+
+            var set2 = new Set<string, string>();
+            set2.SetItem("B");
+            set2.SetItem("C");
+
+            _input.SetItem("S1", set1);
+            _input.SetItem("S2", set2);
+
+            _input.DeleteItem("S2");
+
+            var output = await _subject.SnapshotAsync();
+
+            Assert.AreEqual(2, output.Count);
+            Assert.AreEqual("A", output["A"]);
+            Assert.AreEqual("B", output["B"]);
         }
     }
 }
